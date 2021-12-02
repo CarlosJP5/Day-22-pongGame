86e0a9952cd89b9d0f00c51c335d622162c0e9d1
Carlos J Pacheco <Carlosj.pacheco15@gmail.com>
2021-12-02 10:28:52 -0400

    Pong Game

---
+++ main.py
@@ -0,0 +1,15 @@
+from turtle import Screen
+
+# TODO: Create Screen
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=500)
+screen.title("Pong Game")
+# TODO: Create and move Paddle
+# TODO: Create another paddle
+# TODO: Create de Ball and make move
+# TODO: Detect collision with wall
+# TODO: Detect collision with paddle
+# TODO: Detect when paddle misses
+# TODO: Keep Score
+screen.exitonclick()
