51c3b75e5483c83fff7822962625c710b6a324fd
Carlos J Pacheco <Carlosj.pacheco15@gmail.com>
2021-12-02 17:01:29 -0400

    Hice las paletas y puedo mover una de ellas

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from turtle import Screen
+from paletas import Paddle
 
 # TODO: Create Screen
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(width=800, height=500)
+screen.setup(width=800, height=540)
 screen.title("Pong Game")
+screen.tracer(0)
+screen.listen()
+
 # TODO: Create and move Paddle
+paddle = Paddle()
+screen.onkey(paddle.move_up, "Up")
+screen.onkey(paddle.move_down, "Down")
+
 # TODO: Create another paddle
+# Game Start
+game_on = True
+while game_on:
+    screen.update()
+    # paddle.pc_move()
+
+
+
 # TODO: Create de Ball and make move
 # TODO: Detect collision with wall
 # TODO: Detect collision with paddle
---
+++ paletas.py
@@ -0,0 +1,42 @@
+from turtle import Turtle
+PLAYER_START = [(-360, -20), (-360, 0), (-360, 20)]
+PC_START = [(360, -20), (360, 0), (360, 20)]
+TOP = 250
+BOT = -250
+
+
+class Paddle:
+
+    def __init__(self):
+        self.player = []
+        self.pc = []
+        for position in PLAYER_START:
+            body = Turtle(shape="square")
+            body.color("white")
+            body.penup()
+            body.goto(position)
+            self.player.append(body)
+
+        for positions in PC_START:
+            body = Turtle(shape="square")
+            body.color("white")
+            body.penup()
+            body.goto(positions)
+            self.pc.append(body)
+
+        self.player_head = self.player[0]
+        self.pc_head = self.pc[0]
+
+    def move_up(self):
+        self.player_head = self.player[-1]
+        if self.player_head.ycor() <= TOP:
+            for body in self.player:
+                body.setheading(90)
+                body.forward(20)
+
+    def move_down(self):
+        self.player_head = self.player[0]
+        if self.player_head.ycor() >= BOT:
+            for body in self.player:
+                body.setheading(270)
+                body.forward(20)
